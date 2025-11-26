5967948ada4cbfcda564763400e150bcd2c0f465
xb1002 <2709668323@qq.com>
2025-11-26 20:25:09 +0800

    更新auto因子模版

--- factors/auto.py
+++ factors/auto.py
@@ -8,13 +8,18 @@ import pandas as pd
 import numpy as np
 from factor_engine import register_factor
 
-
+# def factor_func(df: pd.DataFrame) -> pd.Series:
+#     """
+#     这里接受的是一个multi-index为(code, date)的DataFrame
+#     需要返回一个与df索引对齐的Series
+#     """
+    
 # 示例因子1：动量类
 @register_factor(
-    name="momentum_10d",
+    name="momentum_20d",
     required_fields=["close"],
     version="v1"
 )
-def momentum_10d(df: pd.DataFrame) -> pd.Series:
-    """10日动量因子"""
-    return df.groupby(level="code")["close"].pct_change(10)
+def momentum_20d(df: pd.DataFrame) -> pd.Series:
+    """20日动量因子"""
+    return df.groupby(level="code")["close"].pct_change(20)
