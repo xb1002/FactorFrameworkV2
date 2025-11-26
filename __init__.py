df61a7038e4a4961f5d7a8c2f854512768baf627
xb1002 <2709668323@qq.com>
2025-11-26 22:25:06 +0800

    更新导入

--- __init__.py
+++ __init__.py
@@ -1,2 +1,9 @@
+import sys
+from pathlib import Path
+# 添加项目根目录到 sys.path
+project_root = Path(__file__).parent.resolve()
+if str(project_root) not in sys.path:
+    sys.path.insert(0, str(project_root))
+    
 from .app import get_factor_library
 factor_lib = get_factor_library()
